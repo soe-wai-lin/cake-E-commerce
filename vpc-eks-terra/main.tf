dce7ea73640bcaff2022d13d2a20e6da554ac536
swl <swl@gmail.com>
2025-08-06 19:28:52 +0700

    change v1.0

--- vpc-eks-terra/main.tf
+++ vpc-eks-terra/main.tf
@@ -46,7 +46,7 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.32"
+  cluster_version = "1.33"
  
 
   cluster_endpoint_public_access           = true
@@ -62,7 +62,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type = "AL2023_x86_64_STANDARD"
 
   }
 
